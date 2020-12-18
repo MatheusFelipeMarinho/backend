86450bdc5014e6eb69b45db7a00114d1dfdac7b5
MatheusFelipeMarinho <matheus_fellip@hotmail.com>
2020-12-18 13:49:11 -0300

    :construction: Generate Recover password
    Add tests

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,6 +3,8 @@ import { container } from 'tsyringe';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 
+//import IMailProvider from '../providers/MailProvider/models/IMailProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
